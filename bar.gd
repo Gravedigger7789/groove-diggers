8e5176ef739281e375000472d332f51952fe48f0
Chris Racioppo <Gravedigger7789@gmail.com>
2023-11-15 23:06:39 -0500

    fixing hit detection

--- bar.gd
+++ bar.gd
@@ -1,17 +1,22 @@
 extends Node2D
 
-var current_note := {}
+@onready var top_note: RayCast2D = $TopNote
+@onready var bottom_note: RayCast2D = $BottomNote
 
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("hit"):
-		#print (current_note.keys())
-		for note: String in current_note.keys():
-			current_note[note].queue_free()
-			current_note.erase(note)
+func _physics_process(_delta: float) -> void:
+	if Input.is_action_just_pressed("hit"):
+		if top_note.is_colliding():
+			print("top: ", get_raycast_collision_distance(top_note))
+			top_note.get_collider().queue_free()
+		if bottom_note.is_colliding():
+			print("bottom: ", get_raycast_collision_distance(bottom_note))
+			bottom_note.get_collider().queue_free()
 
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	current_note[get_node(area.get_path()).name] = area
-
-
-func _on_area_2d_area_exited(area: Area2D) -> void:
-	current_note.erase(get_node(area.get_path()).name)
+func get_raycast_collision_distance(raycast: RayCast2D) -> float:
+	var collision_distance := -1.0
+	if raycast.is_colliding():
+		var origin := raycast.global_transform.origin
+		var collision_point := raycast.get_collision_point()
+		collision_distance = origin.distance_to(collision_point)
+	return collision_distance
+	
