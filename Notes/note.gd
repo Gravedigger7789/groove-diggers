9b358a7e287ea3e1a89cf1b05b2c7ff095b64aeb
Chris Racioppo <Gravedigger7789@gmail.com>
2023-11-18 15:54:07 -0500

    cleaning up empty spaces

--- Notes/note.gd
+++ Notes/note.gd
@@ -6,7 +6,6 @@ const TOP_SPAWN = Vector2(SPAWN_X, 80)
 const BOTTOM_SPAWN = Vector2(SPAWN_X, 280)
 const SECONDS_TO_TARGET = 2.0
 
-
 var speed : float
 
 @onready var time_start := Time.get_unix_time_from_system()
@@ -33,7 +32,6 @@ func _physics_process(delta: float) -> void:
 		#print("reached target ", Time.get_unix_time_from_system() - time_start)
 		#print(position.x)
 		#queue_free()
-
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
