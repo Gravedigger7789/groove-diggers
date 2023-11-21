b7c60d4c83ee7854c82930052acb0230de42e460
Chris Racioppo <Gravedigger7789@gmail.com>
2023-11-20 20:07:37 -0500

    Fixed raycast collision

--- bar.gd
+++ bar.gd
@@ -5,17 +5,20 @@ extends Node2D
 
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("hit_up"):
-		if top_note.is_colliding():
-			var collision_distance := get_raycast_collision_distance(top_note)
-			var collider := top_note.get_collider()
-			if collider.has_method("hit"):
-				collider.hit(collision_distance)
+		check_for_collision(top_note)
 	if Input.is_action_just_pressed("hit_down"):
-		if bottom_note.is_colliding():
-			var collision_distance := get_raycast_collision_distance(bottom_note)
-			var collider := bottom_note.get_collider()
-			if collider.has_method("hit"):
-				collider.hit(collision_distance)
+		check_for_collision(bottom_note)
+
+func check_for_collision(raycast: RayCast2D) -> void:
+	if raycast.is_colliding():
+		var collision_distance := get_raycast_collision_distance(raycast)
+		var collider := raycast.get_collider()
+		if collider.has_method("hit"):
+			collider.hit(collision_distance)
+		raycast.add_exception(collider)
+		raycast.force_raycast_update()
+	else:
+		raycast.clear_exceptions()
 
 func get_raycast_collision_distance(raycast: RayCast2D) -> float:
 	var collision_distance := -1.0
