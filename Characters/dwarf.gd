6903dfd9c517c8d962fe538702d3e5e04d525449
Chris Racioppo <Gravedigger7789@gmail.com>
2023-11-21 20:36:08 -0500

    Added jump animation

--- Characters/dwarf.gd
+++ Characters/dwarf.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var torso: AnimatedSprite2D = $Torso
 @onready var legs: AnimatedSprite2D = $Legs
 @onready var torch: PointLight2D = $Torch
+@onready var fall_timer: Timer = $FallTimer
 
 var top_position := -100.0
 var middle_position := -40.0
@@ -33,10 +34,16 @@ func _process(_delta: float) -> void:
 		if down_click_time > 0:
 			if now - down_click_time < click_tolerance:
 				torso.play("hit-mid")
+				if legs.animation != ("jump"):
+					legs.play("jump")
 				position.y = middle_position
+				fall_timer.start()
 		else:
 			torso.play("hit-up")
+			if legs.animation != ("jump"):
+				legs.play("jump")
 			position.y = top_position
+			fall_timer.start()
 		up_click_time = now
 
 	if !Input.is_action_pressed("hit_up"):
@@ -46,7 +53,10 @@ func _process(_delta: float) -> void:
 		if up_click_time > 0:
 			if now - up_click_time < click_tolerance:
 				torso.play("hit-mid")
+				if legs.animation != ("jump"):
+					legs.play("jump")
 				position.y = middle_position
+				fall_timer.start()
 		else:
 			torso.play("hit-down")
 			position.y = bottom_position
@@ -55,10 +65,12 @@ func _process(_delta: float) -> void:
 	if !Input.is_action_pressed("hit_down"):
 		down_click_time = 0.0
 
+	if position.y == bottom_position && legs.animation != ("run"):
+		legs.play("run")
 	sync_legs_with_torso()
 
 func sync_legs_with_torso() -> void:
-	if torso.animation == ("run"):
+	if torso.animation == ("run") && legs.animation == ("run"):
 		var current_frame := legs.get_frame()
 		var current_progress := legs.get_frame_progress()
 		torso.set_frame_and_progress(current_frame, current_progress)
@@ -83,8 +95,8 @@ func damage(value: int) -> void:
 
 func _on_torso_animation_finished() -> void:
 	if !dead && (torso.animation.contains("hit") || torso.animation.contains("damage")):
-		var current_frame := legs.get_frame()
-		var current_progress := legs.get_frame_progress()
 		torso.play("run")
-		torso.set_frame_and_progress(current_frame, current_progress)
-		position.y = bottom_position
+		sync_legs_with_torso()
+
+func _on_fall_timer_timeout() -> void:
+	position.y = bottom_position
